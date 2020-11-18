02fd221dcce3de53cfedc08a77f734d50644c958
Igor Vovchenko <vov4enkoi32@gmail.com>
2020-11-18 18:17:59 +0300

    Finished

---
+++ script.js
@@ -0,0 +1,17 @@
+const numerOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", '');//промпт вопроса
+const numerOfFilmsDB = { // создание объекта 
+    count: numerOfFilms, // переменной для ответа
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt("Один из последних просмотренных фильмов?", ''),
+        b = +prompt("На сколько вы его оцените?", ''),
+        c = prompt("Один из последних просмотренных фильмов?", ''),
+        d = +prompt("На сколько вы его оцените?", '');
+numerOfFilmsDB.movies[a] = b;
+numerOfFilmsDB.movies[c] = d;
+
+console.log(numerOfFilmsDB);
